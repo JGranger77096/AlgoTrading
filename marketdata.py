dbcf725245159845ac55491a28a541297f618220
LeBadger06 <59570675+LeBadger06@users.noreply.github.com>
2022-07-11 11:26:30 -0500

    Add files via upload

--- marketdata.py
+++ marketdata.py
@@ -1,13 +1,19 @@
 import json, requests, sqlite3
 import pandas as pd
-#headers =
 
+headers = json.loads(open('Alpaca_Key.txt','r').read())
 data_url = "https://data.alpaca.markets/v1/bars/1Min?symbols="
 
 #Specify the tickers
 tickers = ['AAPL','AMZN','FB','GOOG','NFLX']
 symbols = ",".join(tickers)
 
-r=requests.get()
+r = requests.get(f'{data_url}{symbols}',headers=headers)
+data = r.json()
+print(data)
+#Establish database connection
+#db = sqlite3.connect("alpaca/minutes.db")
+
+#Define a create table function
 
 
