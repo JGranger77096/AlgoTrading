3913c660e535c4c2096981c6c74e2394d7c7065a
LeBadger06 <59570675+LeBadger06@users.noreply.github.com>
2022-07-14 13:04:22 -0500

    Add files via upload

--- marketdata.py
+++ marketdata.py
@@ -2,18 +2,38 @@
 
 import json, requests, sqlite3
 import pandas as pd
+import ssl_patch
+import alpaca_trade_api as tradeapi
 
-headers = json.loads(open('Alpaca_Key.txt','r').read())
-data_url = 'https://paper-api.alpaca.markets/v2/stocks/bars/1Hour?symbols='
+headers = json.loads(open('Alpaca_Req.txt','r').read())
+data_url = 'https://data.alpaca.markets/v2/stocks/bars/1Hour?symbols='
 
 #Specify the tickers
 tickers = ['AAPL','AMZN','FB','GOOG','NFLX']
 symbols = ",".join(tickers)
-print(f'{data_url}{symbols}')
-r = requests.get(f'{data_url}{symbols}', headers=headers, verify=False)
-print('r.content: ', r.content)
 
-data = r.json()
+#lines below don't work
+#r = requests.get(f'{data_url}{symbols}', headers=headers, verify=False)
+#print('r.content: ', r.content)
+#data = r.json()
+
+#Create empty dataframe
+hist_df = pd.DataFrame()
+
+#Try alternative approach to collect historical data
+with ssl_patch.no_ssl_verification():
+    api = tradeapi.REST(key_id=headers['APCA-API-KEY-ID'],
+                            secret_key=headers['APCA-API-SECRET-KEY'],
+                            base_url="https://data.alpaca.markets",
+                            api_version='v2')
+
+    for ticker in tickers:
+        ticker_df = api.get_bars(ticker,'1Min').df
+        ticker_df['stock'] = ticker
+        hist_df = hist_df.append(ticker_df)
+hist_df['timestamp'] = pd.DatetimeIndex(pd.to_datetime(hist_df.index, unit='s',))
+print(hist_df.info())
+hist_df.to_csv('alpaca_multibars.csv')
 
 #Establish database connection
 db = sqlite3.connect("minutes.db")
@@ -32,11 +52,11 @@ def create_table(symbols):
 create_table([ticker for ticker in tickers]) #list comprehension
 
 #Load data into tables
-for k,v in data.items():
-    df = pd.DataFrame(data[k])
-    df.columns = ['timestamp', 'open','high','low','close''volume']
-    df['timestamp'] = pd.DatetimeIndex(pd.to_datetime(df['timestamp'], unit='s',).tz_localize('UTC').tz_convert('America/New_York'))
-    df.to_sql(k, db, if_exists='append', index=False)
-
+hist_df.to_sql(name='hist_data_min', con=db, if_exists='append', index=False)
 db.commit()
+
+#Check database data - write to file
+from_db = pd.read_sql(con=db,sql='SELECT * FROM hist_data_min')
+from_db.to_csv("from_db.csv")
+
 db.close()
